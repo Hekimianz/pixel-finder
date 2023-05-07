f709244be72be12192510c7a073f52e9c891c3f1
Aram Hekimian <aramhek121@gmail.com>
2023-05-06 19:50:46 -0600

    implemented selections on level 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Level1 from "./components/Level1";
 import Level2 from "./components/Level2";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDYpnoo2a9BDgsK5KRZWc9heYNQJi_drzk",
+  authDomain: "retrofinder-60392.firebaseapp.com",
+  projectId: "retrofinder-60392",
+  storageBucket: "retrofinder-60392.appspot.com",
+  messagingSenderId: "124516766966",
+  appId: "1:124516766966:web:ad9af9fb0eb2d4bc3708d7",
+});
+
 function App() {
   return (
     <>
--- src/components/Level1.js
+++ src/components/Level1.js
@@ -1,10 +1,76 @@
 import levelImg from "../1.jpeg";
+import hat from "../hat.png";
+import gollum from "../gollum.png";
+import mirror from "../Mirror.png";
+import jake from "../Jake.png";
+import mojo from "../mojo jojo.png";
 import "./Level1.css";
 export default function Level1() {
+  function imgClickHandler(e) {
+    const div = document.getElementById("menu");
+    if (div.style.display === "") {
+      div.style.display = "flex";
+      let left = e.clientX + 10 + "px";
+      let top = e.clientY + "px";
+      div.style.left = left;
+      div.style.top = top;
+    } else {
+      div.style.display = "";
+    }
+  }
+
+  function selectionClickHandler() {
+    const div = document.getElementById("menu");
+    div.style.display = "";
+  }
+
   return (
     <div id="lvl1--cont">
-      <nav></nav>
-      <img id="lvl1--img" alt="" src={levelImg} />
+      <nav>
+        <div className="selectionPrev">
+          <img className="selectionPrevImg" src={hat} alt="" />
+          <span className="selectionPrevName">Beanie</span>
+        </div>
+        <div className="selectionPrev">
+          <img className="selectionPrevImg" src={mojo} alt="" />
+          <span className="selectionPrevName">Mojo Jojo</span>
+        </div>
+        <div className="selectionPrev">
+          <img className="selectionPrevImg" src={gollum} alt="" />
+          <span className="selectionPrevName">Gollum</span>
+        </div>
+        <div className="selectionPrev">
+          <img className="selectionPrevImg" src={jake} alt="" />
+          <span className="selectionPrevName">Jake</span>
+        </div>
+        <div className="selectionPrev">
+          <img className="selectionPrevImg" src={mirror} alt="" />
+          <span className="selectionPrevName">Mirror</span>
+        </div>
+      </nav>
+      <section id="menu">
+        <span onClick={selectionClickHandler} className="selection">
+          Beanie
+        </span>
+        <span onClick={selectionClickHandler} className="selection">
+          Mojo Jojo
+        </span>
+        <span onClick={selectionClickHandler} className="selection">
+          Gollum
+        </span>
+        <span onClick={selectionClickHandler} className="selection">
+          Jake
+        </span>
+        <span onClick={selectionClickHandler} className="selection">
+          Mirror
+        </span>
+      </section>
+      <img
+        onClick={(e) => imgClickHandler(e)}
+        id="lvl1--img"
+        alt=""
+        src={levelImg}
+      />
     </div>
   );
 }
